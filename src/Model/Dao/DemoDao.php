a24f5aa7c0428e87ab39340a8d867eca0e486da3
梁俊豪 <875342392@qq.com>
2021-08-27 11:35:27 +0800

    调整文件

---
+++ src/Model/Dao/DemoDao.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Demo\Model\Dao;
+
+class DemoDao
+{
+    public function __construct()
+    {
+    }
+
+    public function init()
+    {
+    }
+}
---
+++ src/Model/Logic/DemoLogic.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Demo\Model\Logic;
+
+class DemoLogic
+{
+    public function __construct()
+    {
+    }
+
+    public function init()
+    {
+    }
+}
